437ab6095588f13d25db06d4d4782dc67e18637c
Alexey Litvinov <sullenor@yandex-team.ru>
2016-03-06 09:17:41 +0300

    barely working example

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const dirname = require('path').dirname;
-const forEach = require('lodash');
 const plugin = require('postcss').plugin;
 const postcss = require('postcss');
+const queue = require('async').queue;
 const readFile = require('fs').readFile;
-const readFileSync = require('fs').readFileSync;
 const resolve = require('path').resolve;
 
 const importRegexp = /^:import\((.+)\)$/;
@@ -15,20 +14,88 @@ const nameOfTheCurrentPlugin = 'postcss-modules-resolve-imports';
  * @return {function}
  */
 module.exports = plugin(nameOfTheCurrentPlugin, function postcssModulesResolveImports(opts) {
+  // https://github.com/caolan/async#queueworker-concurrency
+  const q = queue(function worker({ filename, runner, trace }, cb) {
+    readFile(filename, (er, css) => {
+      if (er) {
+        return void cb(er);
+      }
+
+      runner
+        .process(css, {from: filename, trace})
+        .then(result => cb(null, result.root))
+        .catch(cb);
+    });
+  });
+
+  /**
+   * @param {string} filename
+   * @param {object} runner
+   * @param {string} trace
+   */
+  function addToQueue(filename, runner, trace) {
+    return new Promise((resolve, reject) => {
+      q.push({ filename, runner, trace }, (er, tree) => {
+        if (er) {
+          return void reject(er);
+        }
+
+        resolve(tree);
+      });
+    });
+  }
+
+  const cache = {};
+  const traces = {};
+
+  /**
+   * @param  {object} tree   Root node
+   * @param  {object} result
+   * @return {promise}
+   */
   return function resolveImports(tree, result) {
-    const from = tree.source.input.file;
-    const plugins = getPluginsForParse(result.processor.plugins);
+    const plugins = retrievePluginsForParse(result.processor.plugins);
     const runner = postcss(plugins);
 
+    const pending = [];
+    // https://github.com/postcss/postcss/blob/master/docs/api.md#inputfile
+    const sourcePath = tree.source.input.file;
+    const trace = result.opts.trace || String.fromCharCode(0);
+
+    let depNr = 0;
+    // https://github.com/postcss/postcss/blob/master/docs/api.md#containerwalkrulesselectorfilter-callback
     tree.walkRules(importRegexp, rule => {
-      const to = resolve(dirname(from), RegExp.$1.replace(/^["']|["']$/g, ''));
-      const css = readFileSync(to, 'utf8');
-      const root = runner.process(css, {from: to}).root;
-      tree.prepend(root.nodes);
+      const _trace = trace + String.fromCharCode(depNr++);
+      const importsPath = resolveImportsPath(RegExp.$1, sourcePath);
+
+      traces[_trace] = importsPath;
+
+      if (cache[importsPath]) {
+        return cache[importsPath];
+      }
+
+      pending.push(addToQueue(importsPath, runner, _trace)
+        .then(anotherTree => {
+          cache[importsPath] = anotherTree;
+          return anotherTree;
+        }));
+
       tree.removeChild(rule);
     });
 
-    return tree;
+    return Promise.all(pending)
+      .then(_ => {
+        if (trace !== String.fromCharCode(0)) {
+          return _;
+        }
+
+        const files = Object.keys(traces).sort(traceKeySorter).map(t => traces[t]);
+        files.reverse();
+
+        files.forEach(file => tree.prepend(cache[file].nodes));
+
+        return tree;
+      });
   };
 });
 
@@ -36,7 +103,7 @@ module.exports = plugin(nameOfTheCurrentPlugin, function postcssModulesResolveIm
  * @param  {array} plugins
  * @return {array}
  */
-function getPluginsForParse(plugins) {
+function retrievePluginsForParse(plugins) {
   const pluginsForParse = plugins.slice();
   let ln = pluginsForParse.length;
 
@@ -44,125 +111,41 @@ function getPluginsForParse(plugins) {
     pluginsForParse.pop();
   }
 
+  // clear exports
   return pluginsForParse;
 }
 
-// /**
-//  * @param  {object}  [opts]
-//  * @param  {boolean} opts.sync
-//  * @return {function}
-//  */
-// module.exports = plugin(nameOfTheCurrentPlugin, function postcssModulesResolveImports(opts) {
-//   return function resolveImports(tree, result) {
-//     // https://github.com/postcss/postcss/blob/master/docs/api.md#inputfile
-//     // const filepath = tree.source.input.file;
-//     const plugins = getPluginsForParse(result.processor.plugins);
-//     const runner = postcss(plugins);
+/**
+ * @param  {string} importsPath relative
+ * @param  {string} sourcePath  absolute
+ * @return {string}             absolute
+ */
+function resolveImportsPath(importsPath, sourcePath) {
+  // @todo Support node_modules
+  return resolve(dirname(sourcePath), purifyPath(importsPath));
+}
 
-//     const cache = {};
-//     const queue = [];
-//     const traces = {};
+/**
+ * @param  {string} importsPath
+ * @return {string}
+ */
+function purifyPath(importsPath) {
+  return importsPath.replace(/^["']|["']$/g, '');
+}
 
-//     traverseImports(tree, dependency =>
-//       loadDependency(dependency, {cache, queue, runner}));
-
-//     return Promise.all(queue)
-//       .then(_ => combineTrees(tree, cache));
-
-//     // -> traceKeySorter
-//   };
-// });
-
-// /**
-//  * @param  {array} plugins
-//  * @return {array}
-//  */
-// function getPluginsForParse(plugins) {
-//   const pluginsForParse = plugins.slice();
-//   let ln = pluginsForParse.length;
-
-//   while (pluginsForParse[--ln].postcssPlugin !== nameOfTheCurrentPlugin) {
-//     pluginsForParse.pop();
-//   }
-
-//   return pluginsForParse;
-// }
-
-// /**
-//  * @param  {root}     tree
-//  * @param  {function} iterator
-//  */
-// function traverseImports(tree, iterator) {
-//   // https://github.com/postcss/postcss/blob/master/docs/api.md#inputfile
-//   const source = tree.source.input.file;
-//   // https://github.com/postcss/postcss/blob/master/docs/api.md#containerwalkrulesselectorfilter-callback
-//   tree.walkRules(importRegexp, rule => {
-//     iterator(getDepsPath(RegExp.$1, source), source);
-//     tree.removeChild(rule);
-//   });
-// }
-
-// /**
-//  * @param  {string} to
-//  * @param  {string} from
-//  * @return {string}
-//  */
-// function getDepsPath(to, from) {
-//   // @todo support non-relative imports
-//   // const filepath = /\w/i.test(to[0])
-//   //   ? require.resolve(to)
-//   //   : resolve(dirname(from), to);
-//   return resolve(dirname(from), to.replace(/^["']|["']$/g, ''));
-// }
-
-// /**
-//  * @param  {string} dependency
-//  * @param  {object} options.cache
-//  * @param  {array}  options.queue
-//  */
-// function loadDependency(dependency, {cache, queue, runner}) {
-//   const pending = new Promise((resolve, reject) =>
-//     readFile(dependency, 'utf8', (er, source) => {
-//       if (er) {
-//         return void reject(er);
-//       }
-
-//       runner
-//         .process(source, {from: dependency})
-//         .then(result => {
-//           cache[dependency] = result.root;
-
-//           traverseImports(result.root, deps =>
-//             loadDependency(deps, {cache, queue, runner}));
-
-//           resolve(result.root);
-//         })
-//         .catch(reject);
-//     }));
-
-//   queue.push(pending);
-//   return pending;
-// }
-
-// function combineTrees(root, cache) {
-//   forEach(cache, tree => root.prepend.apply(root, tree.nodes));
-//   console.log('test', root)
-//   return root;
-// }
-
-// // Sorts dependencies in the following way:
-// // AAA comes before AA and A
-// // AB comes after AA and before A
-// // All Bs come after all As
-// // This ensures that the files are always returned in the following order:
-// // - In the order they were required, except
-// // - After all their dependencies
-// function traceKeySorter(a, b) {
-//   if (a.length < b.length) {
-//     return a < b.substring(0, a.length) ? -1 : 1;
-//   } else if (a.length > b.length) {
-//     return a.substring(0, b.length) <= b ? -1 : 1;
-//   } else {
-//     return a < b ? -1 : 1;
-//   }
-// };
+// Sorts dependencies in the following way:
+// AAA comes before AA and A
+// AB comes after AA and before A
+// All Bs come after all As
+// This ensures that the files are always returned in the following order:
+// - In the order they were required, except
+// - After all their dependencies
+function traceKeySorter(a, b) {
+  if (a.length < b.length) {
+    return a < b.substring(0, a.length) ? -1 : 1;
+  } else if (a.length > b.length) {
+    return a.substring(0, b.length) <= b ? -1 : 1;
+  } else {
+    return a < b ? -1 : 1;
+  }
+};
