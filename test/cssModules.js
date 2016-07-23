35bdda0917f40305249e7489ca089ff501d0ef60
Alexey Litvinov <sullenor@yandex-team.ru>
2016-07-23 23:41:58 +0300

    the new test runner

--- test/cssModules.js
+++ test/cssModules.js
@@ -1,22 +1,30 @@
+'use strict';
+
+const { plugin } = require('postcss');
+const { relative } = require('path');
 const postcss = require('postcss');
-const relative = require('path').relative;
 
 const LocalByDefault = require('postcss-modules-local-by-default');
 const ExtractImports = require('postcss-modules-extract-imports');
 const Scope = require('postcss-modules-scope');
-const ResolveImports = require('../index');
+// const ResolveImports = require('../index');
 const ExtractExports = require('postcss-modules-extract-exports');
 const Values = require('postcss-modules-values');
 
-global.assert = require('assert');
-global.runner = function () {
-  return postcss([
+module.exports = plugin('css-modules', function initCssModules() {
+  const runner = postcss([
     Values,
     LocalByDefault,
     ExtractImports,
     new Scope({generateScopedName: (local, filename) =>
       Scope.generateScopedName(local, relative(process.cwd(), filename))}),
-    ResolveImports,
+    // ResolveImports,
     ExtractExports,
   ]);
-};
+
+  return cssModules;
+
+  function cssModules(css) {
+    return runner.process(css, {from: css.source.input.file}).css;
+  }
+});
