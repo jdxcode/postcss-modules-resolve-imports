465fbba6965e52a27b9f453d1cb318ee37cd1f81
Alexey Litvinov <sullenor@yandex-team.ru>
2017-02-28 22:17:11 +0300

    plugin notes

--- sync.js
+++ sync.js
@@ -1,17 +1,20 @@
 'use strict';
 
+/**
+ * Notes about postcss plugin's api
+ *
+ * Containers are iterated with .walk* methods.
+ * Rules are actually selectors.
+ * Decls are actually css rules (keys prop, value).
+ */
+
 const {plugin} = require('postcss');
 
 module.exports = plugin('postcss-modules-resolve-imports', resolveImportsPlugin);
 
-function resolveImportsPlugin({resolve}) {
+function resolveImportsPlugin({resolve} = {}) {
   // normalize opts
   return resolveImports;
 
-  function resolveImports(root, result) {
-    // walk imports, memorise the files, build graph, remove imports
-    // resolve dependency order
-    // [resolve tokens, merge trees]
-    // provide exports
-  }
+  function resolveImports(ast, result) {}
 }
